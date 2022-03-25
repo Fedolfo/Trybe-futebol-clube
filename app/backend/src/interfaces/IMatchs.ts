58f075024c5c0fed31b0262215a1358fbbc6aff4
Filipe <filipecandido123@hotmail.com>
2022-03-25 01:26:46 -0300

    refactor: interface match

--- app/backend/src/interfaces/IMatchs.ts
+++ app/backend/src/interfaces/IMatchs.ts
@@ -1,9 +1,16 @@
-export default interface IMatchs {
+interface IIDMatch {
   id: number;
+}
+
+export interface IMatchBody {
   homeTeam: number;
   homeTeamGoals: number;
   awayTeam: number;
+  awayTeamGoals: number;
   inProgress: boolean;
+}
+
+export interface IMatchsDTO extends IIDMatch, IMatchBody {
   homeClub: {
     clubName: string;
   }
