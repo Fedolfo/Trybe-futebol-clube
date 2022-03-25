9a4f24c886c28293aedc52bc88a094439ba824fe
Filipe <filipecandido123@hotmail.com>
2022-03-25 01:26:27 -0300

    feat: adiciona validaçao de times para nao ser iguais na criaçao de partida

--- app/backend/src/controllers/Matchs.ts
+++ app/backend/src/controllers/Matchs.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from 'express';
+import { ClubService } from '../services';
 import MatchService from '../services/Matchs';
 
 class MatchController {
@@ -6,6 +7,38 @@ class MatchController {
     const { inProgress } = req.query;
     const matchs = await MatchService.getMatchs(inProgress as string);
     res.status(200).json(matchs);
+  }
+
+  static async createMatch(req: Request, res: Response) {
+    const { homeTeam, homeTeamGoals, awayTeam, awayTeamGoals, inProgress } = req.body;
+
+    const match = await MatchService
+      .createMatch({ homeTeam, homeTeamGoals, awayTeam, awayTeamGoals, inProgress });
+
+    const idHomeTeam = await ClubService.getByIdClub(match.homeTeam);
+    const idAwayTeam = await ClubService.getByIdClub(match.awayTeam);
+    const CLUB_A = idHomeTeam?.getDataValue('id') as number;
+    const CLUB_B = idAwayTeam?.getDataValue('id') as number;
+
+    if (!CLUB_A || !CLUB_B) {
+      return res.status(401).json({ message: 'Team not found' });
+    }
+
+    if (CLUB_A === CLUB_B) {
+      return res.status(401).json({
+        message: 'It is not possible to create a match with two equal teams' });
+    }
+
+    res.status(201).json(match);
+  }
+
+  static async matchInsertedProgress(req: Request, res: Response) {
+    const { id } = req.params;
+    // const { inProgress } = req.body;
+
+    const match = await MatchService.matchInsertedProgress(Number(id));
+
+    res.status(200).json(match);
   }
 }
 
