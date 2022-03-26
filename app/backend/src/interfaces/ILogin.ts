71ee74fe17ef8c9e6592ba025220b520b7feedb9
Filipe <filipecandido123@hotmail.com>
2022-03-26 17:25:19 -0300

    feat: adiciona interface de contrato para services/login

--- app/backend/src/interfaces/ILogin.ts
+++ app/backend/src/interfaces/ILogin.ts
@@ -1,4 +1,10 @@
-export interface IEmailAndPasswordDTO {
+export interface Ilogin extends ILoginEmailAndPasswordDTO {
+  id: number;
+  username: string;
+  role: string;
+}
+
+export interface ILoginEmailAndPasswordDTO {
   email: string;
   password: string;
 }
