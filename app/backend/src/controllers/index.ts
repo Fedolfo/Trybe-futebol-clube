6fd893a353b794670cba7330a2326a411f695319
Filipe <filipecandido123@hotmail.com>
2022-03-26 17:08:30 -0300

    feat: implementa contrato para clubs

--- app/backend/src/controllers/index.ts
+++ app/backend/src/controllers/index.ts
@@ -1,5 +1,6 @@
 import LoginController from './Login';
 import ClubsController from './Clubs';
 import MatchController from './Matchs';
+import LeaderBoardController from './Leaderboard';
 
-export { LoginController, ClubsController, MatchController };
+export { LoginController, ClubsController, MatchController, LeaderBoardController };
