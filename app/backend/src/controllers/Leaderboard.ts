c079f5c67b34e7869f00b905a227b42f6eedcd0c
Filipe <filipecandido123@hotmail.com>
2022-03-26 17:07:03 -0300

    feat: retirado a implementaçao de leaderBoard

--- app/backend/src/controllers/Leaderboard.ts
+++ app/backend/src/controllers/Leaderboard.ts
@@ -1,11 +1,4 @@
-import { Request, Response } from 'express';
-import { Leaderboard } from '../services';
-
 class LeaderBoardController {
-  static async getTeam(_req: Request, res: Response) {
-    const team = await Leaderboard.Team;
-    res.status(200).json(team);
-  }
 }
 
 export default LeaderBoardController;
